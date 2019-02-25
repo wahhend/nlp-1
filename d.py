edc94a65e86afd9b405dd648df6e7914f7d0d9f2
Wahyu Hendro Hartono <whendrohartono@gmail.com>
2019-02-25 10:05:58 +0700

    d done

--- d.py
+++ d.py
@@ -10,14 +10,7 @@ subtitle = re.sub('<\/[a-z]>', '', subtitle)
 
 matches = re.finditer(regex, subtitle, re.MULTILINE)
 
-for matchNum, match in enumerate(matches, start=1):
-    
-    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
-    for groupNum in range(0, len(match.groups())):
-        groupNum = groupNum + 1
-        
-        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+d_subtitle = open('d_subtitle.txt', 'w')
 
-with open('subtitle-clean.srt', 'w') as file:
-    file.write(subtitle)
+for idx, match in enumerate(matches, start=1):
+    d_subtitle.write(match.group(1)+'\n')
